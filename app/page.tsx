07b8314772be2ffda91770b001d521b568d2c616
AbhishekS04 <abhishek23main@gmail.com>
2026-07-13 23:58:04 +0530

    chore: set dynamic rendering to force-dynamic for home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { Artwork } from "./components/MasonryGrid";
 import { sanityFetch, SanityLive } from "../sanity/lib/live";
 import { artworksQuery } from "../sanity/lib/queries";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const { data } = await sanityFetch({ query: artworksQuery });
   const artworks = (data ?? []) as Artwork[];
